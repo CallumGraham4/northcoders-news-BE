779c2cef34f8c740a856945b082e4061f2150a9e
c4allum <callumjohngraham@hotmail.co.uk>
2025-04-29 10:49:09 +0100

    complete task 2

--- api.js
+++ api.js
@@ -1,11 +1,16 @@
 const db = require("./db/connection");
 const express = require("express");
 const app = express();
-const {getApi} = require('./controller/controller')
-
-app.use(express.json());
+const {getApi, getTopics} = require('./controller/controller')
 
 app.get("/api", getApi)
+
+app.get("/api/topics", getTopics)
+
+app.use((req, res) => {
+  res.status(404).send({ msg: "404 error: not found" });
+});
+
 
 
 module.exports = app;
