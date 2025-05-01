7d0051638c3f58ebf4b4662ecebae35163c7c076
c4allum <callumjohngraham@hotmail.co.uk>
2025-05-01 11:48:17 +0100

    complete task 5

--- model/model.js
+++ model/model.js
@@ -44,4 +44,27 @@ const selectArticles = () => {
         })
 }
 
-module.exports = {selectTopics, selectArticleById, selectArticles}
+
+const selectCommentsByArticleId = (article_id) => {
+    if (Number(article_id) && Number(article_id) < 18) {
+    return db
+    .query(`SELECT * FROM comments WHERE article_id = $1
+        ORDER BY comments.created_at DESC;`, [article_id])
+    } else {
+    return db
+    .query(`SELECT * FROM comments WHERE article_id = $1
+            ORDER BY comments.created_at DESC;`, [article_id])
+    .then((result) => {
+        if (result.rows.length === 0) {
+
+            return Promise.reject({status: 404, message: 'Not found: id 1000000 is out of range'})
+    
+            } 
+        return result
+    })
+}}
+
+
+
+
+module.exports = {selectTopics, selectArticleById, selectArticles, selectCommentsByArticleId}
