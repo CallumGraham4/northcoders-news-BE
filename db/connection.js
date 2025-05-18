08fbba968991adedf1241386606d7d6061b05682
c4allum <callumjohngraham@hotmail.co.uk>
2025-05-18 19:19:04 +0100

    Complete hosting instructions with updated seed-prod in package.json and corrected connection.js

--- db/connection.js
+++ db/connection.js
@@ -4,13 +4,13 @@ const ENV = process.env.NODE_ENV || "development";
 
 require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
 
-const db = new Pool();
+//const db = new Pool();
 
-if (!process.env.PGDATABASE) {
-  throw new Error("No PGDATABASE configured");
-} else {
-  console.log(`Connected to ${process.env.PGDATABASE}`);
-}
+// if (!process.env.PGDATABASE) {
+//   throw new Error("No PGDATABASE configured");
+// } else {
+//   console.log(`Connected to ${process.env.PGDATABASE}`);
+// }
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
@@ -24,4 +24,4 @@ if (ENV === "production") {
 }
 module.exports = new Pool(config);
 
-module.exports = db;
+//module.exports = db;
